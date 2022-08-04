85716bdace1a49bc68ebd5aa2c1c8176a161ebc5
Zkeq <learnonly@learnonly.xyz>
2022-08-04 07:55:36 +0000

    2022.8.4 创建三个页面和路由

---
+++ mangosteen/src/views/Statistics.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Statistics.VUE
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style lang="scss" scoped>
+
+</style>

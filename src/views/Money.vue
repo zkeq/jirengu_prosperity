cc2d8ca6398430eaf302b8def0b99a0d194d460a
Zkeq <learnonly@learnonly.xyz>
2022-08-05 13:54:32 +0000

    重构 移动到文件夹

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Layout class-prefix="layout">
-      
+
       <NumberPad />
 
       <Types />
@@ -15,10 +15,10 @@
 </template>
 
 <script lang="ts">
-import NumberPad from "@/components/NumberPad.vue";
-import Types from "@/components/Types.vue";
-import Notes from "@/components/Notes.vue";
-import Tags from "@/components/Tags.vue";
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import Notes from "@/components/Money/Notes.vue";
+import Tags from "@/components/Money/Tags.vue";
 export default {
   name: "Money",
   components: { NumberPad, Types, Notes, Tags },

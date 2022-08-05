5ef5d531cbef51e3e28beb0a671b29a0e7279f0f
Zkeq <learnonly@learnonly.xyz>
2022-08-05 03:29:07 +0000

    将 icon 封装成组件 使用 `svg-sprite-loader` 来处理svg

---
+++ src/components/Icon.vue
@@ -0,0 +1,34 @@
+<template>
+    <svg class="icon">
+        <use :xlink:href="'#' + name" />
+    </svg>
+</template>
+
+<script lang="ts">
+    let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+    // 这玩意是搜的全网搜到的？
+    try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+    // 如果不加 try，在单元测试的时候可能会遇到问题
+
+    export default {
+        name: 'Icon',
+        props: ['name']
+        // props: {
+        //     name: {
+        //         type: String,
+        //         required: true
+        //     }
+        // }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .icon {
+        width: 1em;
+        height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="nav">
-      <router-link to="/money">记账</router-link>
+      <router-link to="/money">
+        <Icon name="Money"/>
+      </router-link>
       |
-      <router-link to="/lables">标签</router-link>
+      <router-link to="/lables">
+        <Icon name="Labels"/>
+      </router-link>
       |
-      <router-link to="/statistics">统计</router-link>
+      <router-link to="/statistics">
+        <Icon name="Statistics"/>
+      </router-link>
     </div>
 </template>
 
@@ -12,7 +18,7 @@
     import Vue from 'vue'
 
     export default Vue.extend({
-        
+        name: 'Nav'
     })
 </script>
 

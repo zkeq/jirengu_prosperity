d6319af06791a4fc41ebaa9af46c899fd2b578d6
Zkeq <learnonly@learnonly.xyz>
2022-08-05 05:38:56 +0000

    改变图标样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -37,10 +37,12 @@
         align-items: center;
         flex-direction: column;
         .icon {
+          > svg {
+            stroke: red;
+          }
           width: 1.3em;
           height: 1.3em;
           vertical-align: -0.15em;
-          fill: currentColor;
           overflow: hidden;
         }
       }

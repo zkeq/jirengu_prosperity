c7e186479e712bddae0ef8a8dc2b9eaff52c3277
Zkeq <learnonly@learnonly.xyz>
2022-08-06 05:39:56 +0000

    js 转 ts

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,33 +1,36 @@
 <template>
   <div>
     <Layout class-prefix="layout">
-
       <NumberPad />
 
       <Types />
 
       <Notes />
 
-      <Tags :data-source.sync="tags"/>
-
+      <Tags :data-source.sync="tags" />
     </Layout>
   </div>
 </template>
 
-<script lang="js">
+<script lang="ts">
+import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
-export default {
-  name: "Money",
-  components: { NumberPad, Types, Notes, Tags },
-  data(){
-    return {
-       tags: ["衣", "食", "住", "行", "123", "456", "789", "0"]
-    }
-  }
-};
+import Component from "vue-class-component";
+
+@Component({
+  components: {
+    NumberPad,
+    Types,
+    Notes,
+    Tags,
+  },
+})
+export default class Money extends Vue {
+  tags = ["衣", "食", "住", "行", "123", "456", "789", "0"];
+}
 </script>
 
 <style lang="scss">

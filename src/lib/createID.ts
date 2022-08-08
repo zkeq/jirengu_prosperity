dec43ca080ccca2bed45b52090b7f8fd95cb5f80
Zkeq <learnonly@learnonly.xyz>
2022-08-08 08:42:52 +0000

    del UUID

--- src/lib/createID.ts
+++ src/lib/createID.ts
@@ -1,11 +1,19 @@
 function createID(){
-    let dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let id = "";
-    for(var i = 0; i < 10; i++){
-        id += dict[Math.floor(Math.random() * dict.length)];
-    }
+    // let dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    // let id = "";
+    // for(var i = 0; i < 10; i++){
+    //     id += dict[Math.floor(Math.random() * dict.length)];
+    // }
 
-    return id
+    // Math.random().toString(36).substr(2, 10);
+
+    // 生成 UUID
+    // return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    //     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    //     return v.toString(16);
+    // })
+
+    return Math.random().toString(36).substr(2, 10);
 }
 
 export default createID;
